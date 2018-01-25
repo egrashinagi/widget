3c0d7a52aa9f5f9a28451b01ccd95d766a758758
Egrashina Galina <egrashina.gi@gmail.com>
2018-01-25 15:30:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Widget
+
+## Description
+
+* Необходимо сделать верстку по макету, используя БЭМ
+* Реализовать widget
+
+### Start development
+Run in development mode **http://localhost:9000**
+
+```
+$ npm start
+
+```
+### Build
+Build project
+
+```
+$ npm run build
+
+```
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: '/images/'
+            }
+          }]
+      }]
+  },
+  plugins: [
+    new ExtractTextPlugin('index.css'),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
+};
